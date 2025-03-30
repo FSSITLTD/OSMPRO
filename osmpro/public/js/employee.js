6d4f51f69b1a969cdcc241341f2ea2bbfe280fcd
FSSITLTD <admin@fssit.uk>
2025-03-30 20:55:46 +0100

    read only removed

--- osmpro/public/js/employee.js
+++ osmpro/public/js/employee.js
@@ -1,7 +1,6 @@
 frappe.ui.form.on("Employee",  {
     date_of_birth (frm){
-        cur_frm.set_df_property("age","read_only",1);
-        var birthdate = frm.doc.date_of_birth; 
+                var birthdate = frm.doc.date_of_birth; 
 
 
     var dobDate = new Date(birthdate);
