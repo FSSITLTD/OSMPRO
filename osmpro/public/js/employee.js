52f46eaec6462ae114475da5385d332f30c5df1c
FSSITLTD <admin@fssit.uk>
2025-03-30 15:46:30 +0100

    Update employee.js

--- osmpro/public/js/employee.js
+++ osmpro/public/js/employee.js
@@ -1,8 +1,14 @@
-frappe.ui.form.on("Employee", "age", function(frm) {
-    var dob = new Date(frm.doc.date_of_birth);
-    var now = new Date();
-    var age_now = now.getFullYear() - dob.getFullYear();
-  
-    cur_frm.set_value("age", age_now);
-    cur_frm.refresh();
-  });
+frappe.ui.form.on("Employee",  {
+    date_of_birth (frm){
+        var birthdate = frm.doc.date_of_birth; 
+
+
+    var dobDate = new Date(birthdate);
+    var today = new Date();
+    var age = today.getFullYear() -dobDate.getFullYear();
+    if (today.getMonth() < dobDate.getMonth() || (today.getMonth() == dobDate.getMonth() && today.getDate() < dobDate.getDay()))
+        age--;
+    frm.set_value("age",age);
+    }
+
+     });
