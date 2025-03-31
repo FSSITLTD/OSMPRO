bc14f62d220af7e4c86bf3da623fec688a6b836c
FSSITLTD <admin@fssit.uk>
2025-03-31 21:19:16 +0100

    spell error

--- osmpro/public/js/employee.js
+++ osmpro/public/js/employee.js
@@ -10,7 +10,7 @@ frappe.ui.form.on("Employee",  {
         age--;
     
     frm.set_value("age",age);
-    frm.set_value("orm-name",birthdate);
+    frm.set_value("form-name",birthdate);
 
 
     }
