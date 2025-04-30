3b9f86544fa80e6d2831f5ee245f86f14424f08c
FSSITLTD <admin@fssit.uk>
2025-04-30 10:12:26 +0100

    added title script

--- osmpro/public/js/employee.js
+++ osmpro/public/js/employee.js
@@ -1,11 +1,12 @@
 frappe.ui.form.on("Employee",  {
     date_of_birth (frm){
                 var birthdate = frm.doc.date_of_birth; 
-
+                var formname = frm.doc.employee_name && frm.doc.employee_number;
 
     var dobDate = new Date(birthdate);
     var today = new Date();
     var age = today.getFullYear() -dobDate.getFullYear();
+   
     if (today.getMonth() < dobDate.getMonth() || (today.getMonth() == dobDate.getMonth() && today.getDate() < dobDate.getDay()))
         age--;
     
