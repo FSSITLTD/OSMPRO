49142afbbbff6eeebf892a92b4fc2211483a9b1b
Jason Tattersall (JST994) <jtattersall@FSSIT.UK>
2025-05-06 15:20:17 +0100

    Separate function

--- osmpro/public/js/employee.js
+++ osmpro/public/js/employee.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("Employee",  {
     date_of_birth (frm){
                 var birthdate = frm.doc.date_of_birth; 
                 var formname = frm.doc.employee_name && frm.doc.employee_number;
-                frm.doc.employee_name && frm.doc.employee_number == formname;
+             
     var dobDate = new Date(birthdate);
     var today = new Date();
     var age = today.getFullYear() -dobDate.getFullYear();
@@ -18,3 +18,21 @@ frappe.ui.form.on("Employee",  {
     }
 
      });
+     frappe.ui.form.on("Employee",  {
+        employee_name(frm){
+                
+                    var formname = frm.doc.employee_name && frm.doc.employee_number;
+                 
+       
+        
+       
+        
+        
+        frm.set_value("age",age);
+        frm.set_value("formname",formname,birthdate);
+        frm.set_value("formname",formname);
+    
+    
+        }
+    
+         });
